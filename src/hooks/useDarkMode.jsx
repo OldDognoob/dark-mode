cd66726ff1f1e4ef7d2546fdeac6eae9b98f7ece
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-18 15:38:24 +0200

    changes

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import  useLocalStorage  from "./useLocalStorage";
+import  {useLocalStorage}  from "./useLocalStorage";
 
 export const useDarkMode = () => {
     const [darkMode, setDarkMode] = useLocalStorage("enabled");
